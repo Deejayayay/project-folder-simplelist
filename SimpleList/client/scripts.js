b2f0488b4fbf745684f956cee970d0d21dcccfdf
Deejayayay <olad@spu.edu>
2023-11-07 17:18:30 -0800

    pau

---
+++ SimpleList/client/scripts.js
@@ -0,0 +1,92 @@
+const http = new coreHTTP;
+
+// Block Variables
+let theList = [];
+
+// setup selectors
+const result = document.querySelector(".result");
+const input =  document.querySelector("#listitem");
+const addButton =  document.querySelector(".add-btn");
+const delButton =  document.querySelector(".del-btn");
+
+// Listeners
+addButton.addEventListener("click", httpPost);
+delButton.addEventListener("click", httpDelete);
+
+/* Helper Functions */
+function ShowList() {
+  let output = "<ul>";
+  console.log(theList);
+  for (const itm of theList) {
+    output += `<li>${itm}</li>`;
+  }
+  output += "</ul>";
+  result.innerHTML = output;
+}
+
+//todo get and write
+async function GetList() {
+  try {
+    const response = await http.get("http://localhost:5500/api");
+    console.log(response);
+    theList = await response;
+    ShowList();
+  } catch (error) {
+    console.log(error);
+  } 
+}
+
+//writes data to the server
+async function WriteList() {
+  try {
+    const response = await http.post("http://localhost:5500/api", theList);
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+    
+}
+
+//todo post and delete
+/* Listener Functions */
+async function httpPost(e) {
+  e.preventDefault();
+  theList.push(input.value);
+  ShowList();
+  await WriteList();
+}
+
+function httpDelete(e) {
+  let index = theList.indexOf(input.value);
+  if(index !== -1) {
+    theList.splice(index, 1);
+    ShowList();
+    WriteList();
+    return;
+  }
+  else {
+    console.log("Item not found");
+  }
+  // e.preventDefault();
+  // theList.pop();
+  // ShowList();
+  // WriteList();
+}
+
+// Loading functions
+function showLoading() {
+  result.innerHTML = "Loading...";
+}
+
+async function main() {
+  addButton.disabled = true;
+  delButton.disabled = true;
+  showLoading();
+
+  await GetList();
+
+  addButton.disabled = false;
+  delButton.disabled = false;
+}
+
+main();
---
+++ SimpleList/filemgr.js
@@ -0,0 +1,36 @@
+//why is this here
+const { json } = require("express");
+const { write } = require("fs");
+const fs = require("fs/promises");
+const path = require("path");
+
+const filepath= "./listdata.json";
+  // Make sure the file exists
+  // Read the file
+  // convert the buffer to a json object and return it
+async function ReadData() {
+  try {
+    if(!fs.access(filepath, fs.constants.F_OK | fs.constants.W_OK | fs.constants.R_OK)){
+      return null;
+    } else {
+      const dataIn = await fs.readFile(filepath);
+      console.log(JSON.parse(dataIn));
+      return JSON.parse(dataIn);
+    }
+  } catch (error) {
+    console.log(error);
+    return [];
+  } 
+}
+
+async function WriteData(dataOut) {
+  try {
+    await fs.writeFile(filepath, JSON.stringify(dataOut));
+    return ;
+  } catch (error) {
+    return ;
+  }
+}
+
+exports.ReadData = ReadData;
+exports.WriteData = WriteData;
